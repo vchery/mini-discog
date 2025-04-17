ddfc526a7c361db14d7f0188716854025759b878
valerie <87587798+vchery@users.noreply.github.com>
2025-04-17 11:48:17 -0400

    added more images, artist page route

--- FlaskProject/app.py
+++ FlaskProject/app.py
@@ -4,13 +4,22 @@ import db
 app = Flask(__name__)
 
 @app.route('/')
-def index():  # put application's code here
+def index():
     # display artist names
-    return render_template('home.html')
+    return render_template('index.html')
+
+@app.route('/artist')
+def artist():
+    # display artist's albums
+    return render_template('artist.html')
 
 @app.route('/albums')
 def albums():
     return render_template('albums.html', albums = db.show_details())
+
+@app.route('/album/<int:album_id>')
+def album(album_id):
+    return db.show_details()[album_id] == 813
 
 if __name__ == '__main__':
     app.run()
--- FlaskProject/schema.sql
+++ FlaskProject/schema.sql
@@ -1,2 +1,16 @@
+CREATE TABLE ariana_grande (
+  track int,
+  title text,
+  album text,
+  album_edition text,
+  album_id int,
+  release_date date,
+  duration interval,
+  genre text,
+  PRIMARY KEY (track, album_id)
+);
+
 select *
 from ariana_grande;
+
+DROP TABLE ariana_grande;
